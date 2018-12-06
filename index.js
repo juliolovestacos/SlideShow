d5d815937b599e46ea12dd3a04bfdaaf32936eae
juliolovestacos <43449724+juliolovestacos@users.noreply.github.com>
2018-12-06 11:36:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -41,3 +41,10 @@ var previousButton = document.getElementById("previous");
 
 previousButton.addEventListener("click",back,false);
 nextButton.addEventListener("click",next,false); 
+function autoSlide(){
+if (document.getElementById("auto").checked)
+ next(); 
+}
+
+
+setInterval(autoSlide,2000); // Next
